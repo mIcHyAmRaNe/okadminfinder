7e25732c8be8e5038c9d7d9a4fcddf2c77be2dfe
michyamrane <michyamrane@gmail.com>
2024-07-08 15:13:15 +0200

    debian package

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
+import os
 
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
+def get_files(directory, filetypes):
+    return [os.path.join(directory, f) for f in os.listdir(directory) if any(f.endswith(ft) for ft in filetypes)]
+
 setup(
     name="okadminfinder",
     version="1.1.0",
-    description="[ Admin panel finder / Admin Login Page Finder ] ¢σ∂є∂ ву 👻 (❤-❤) 👻",
+    description="[Admin panel - Directories - Subdomains] finder ¢σ∂є∂ ву 👻 (❤-❤) 👻",
     author="mIcHyAmRaNe",
-    author_email="marseillaisanonymous@gmail.com",
+    author_email="6m63er17c@mozmail.com",
     license="Apache-2.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -33,6 +37,10 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),
     include_package_data=True,
+    data_files=[
+        ('/usr/share/okadminfinder', ['LICENSE', 'README.md']),
+        ('/usr/share/okadminfinder/LinkFile', get_files('okadminfinder/LinkFile', ['txt'])),
+    ],
     python_requires=">=3.7",
     install_requires=[
         "argparse>=1.4.0",
